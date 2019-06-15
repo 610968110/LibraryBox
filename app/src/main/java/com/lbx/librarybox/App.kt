ad2c9ba33b7a78b607f1bbb8280fd4f630ec4b54
liboxin <610968110@qq.com>
2019-06-15 15:12:53 +0800

    init

---
+++ app/src/main/java/com/lbx/librarybox/App.kt
@@ -0,0 +1,19 @@
+package com.lbx.librarybox
+
+import android.app.Application
+
+/**
+ * Copyright © 2013-2019 Worktile. All Rights Reserved.
+ * Author: liboxin
+ * Email: liboxin@worktile.com
+ * Date: 2019/6/15
+ * Time: 15:02
+ * Desc:
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
